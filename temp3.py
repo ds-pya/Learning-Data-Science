14d575d9c75b65744be266be0f86d07cbdd4a368
ds-pya <164458916+ds-pya@users.noreply.github.com>
2025-04-21 10:23:33 +0900

    temp3.py 업데이트

--- temp3.py
+++ temp3.py
@@ -1,8 +1,20 @@
-package com.yourpkg.log
+from datasets import concatenate_datasets, DatasetDict
 
-import androidx.fragment.app.Fragment
+# 기존 KLUE-NER, CoNLL2003 토큰화 결과
+# tokenized_klue = ...
+# tokenized_conll = ...
 
-data class LogLine(
-    val message: String,
-    val destination: Class<out Fragment>? = null
-)
+# 각 split을 합치자 (train + validation)
+merged_dataset = DatasetDict({
+    "train": concatenate_datasets([tokenized_klue["train"], tokenized_conll["train"]]),
+    "validation": concatenate_datasets([tokenized_klue["validation"], tokenized_conll["validation"]])
+})
+
+# DataLoader 재정의
+from torch.utils.data import DataLoader
+from transformers import DataCollatorForTokenClassification
+
+collator = DataCollatorForTokenClassification(tokenizer)
+
+train_loader = DataLoader(merged_dataset["train"], batch_size=32, shuffle=True, collate_fn=collator)
+eval_loader = DataLoader(merged_dataset["validation"], batch_size=32, shuffle=False, collate_fn=collator)
